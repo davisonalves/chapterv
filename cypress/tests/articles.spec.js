af541feac0376f6e3ff75b96c9c31c5216e5bd41
Davison Alves <53572219+davisonalves@users.noreply.github.com>
2021-10-26 15:23:37 -0400

    refactor: using page objects in article testing

--- cypress/tests/articles.spec.js
+++ cypress/tests/articles.spec.js
@@ -1,5 +1,6 @@
 /// <reference types="cypress" />
 /// <reference types="../support/" />
+import articles from "../support/pages/articles"
 
 describe('Articles', () => {
 
@@ -10,16 +11,9 @@ describe('Articles', () => {
 
     it('Cadastro de um novo artigo com sucesso', () => {
 
-        const articleName = 'Artigo de testes' + new Date().getTime();
+        articles.acessarFormulario();
+        articles.preencherFormulario();
+        articles.verificarSeOArtigoFoiCriado();
 
-        cy.get('[href*=editor]').click(); //tudo que contenha editor
-        cy.get('[ng-model$=title]').type(articleName); //tudo que termine com title
-        cy.get('[ng-model$=description]').type('Descrição do artigo de testes'); 
-        cy.get('[ng-model$=body]').type('Corpo do texto do artigo de testes'); 
-        cy.get('[ng-model$=tagField]').type('cypress'); 
-
-        cy.contains('button', 'Publish Article').click();
-
-        cy.get('h1').should('have.text', articleName);
     });
 });
